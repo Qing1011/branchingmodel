ae2a260a48d216e0fe517d70ec0900d128893a9d
Qing <allison.yao1011@gmail.com>
2023-03-20 11:16:15 -0400

    update codes

--- codes/negative_binomial.py
+++ codes/negative_binomial.py
@@ -8,5 +8,13 @@ def negative_binomial(n, p, size=None):
     # return: samples   
     return np.random.negative_binomial(n, p, size)
 
+def ising_model(n, beta, size=None):
+    # Sample from an Ising model.
+    # n: number of spins
+    # beta: inverse temperature
+    # size: number of samples
+    # return: samples
+    return np.random.binomial(1, 1 / (1 + np.exp(-2 * beta)), size=(size, n))
+
 
 
