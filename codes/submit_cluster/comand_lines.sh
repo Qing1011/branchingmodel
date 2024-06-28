2d2bd7570c6960ef721c4f3fdf1110b42865c013
Qing <allison.yao1011@gmail.com>
2024-06-28 10:36:21 -0400

    model comparison
    updates the codes and figures of model comparison

--- codes/submit_cluster/comand_lines.sh
+++ codes/submit_cluster/comand_lines.sh
@@ -27,16 +27,16 @@ do
 mkdir "branching_R0-5.5_r-${i}"; 
 done
 
-for i in 0 1 2 3;
+for i in 0 1 2 3 4 5 6 7 8;
 do
    qsub branching_${i}.pbs;
 done
 
 
 
-for i in 1.0 0.5 0.2 0.1 0.05 5.0 2.5 3.333 1.333 0.667 0.37 0.286 0.133 0.067 0.033; 
+for i in 1 0.5 0.2 0.1 0.05 0.025; 
 do
-  tar -czvf branching_R0-5.5_r-${i}.tar.gz branching_R0-5.5_r-${i}/
+  tar -czvf branching_R0-2.5_r-${i}.tar.gz branching_R0-2.5_r-${i}/
 done
 
 
